c140e76df26eb1acd2cdadb0cfc48273fec1b5c8
Shoga Ayodeji <86511436+Hayordeji@users.noreply.github.com>
2024-10-20 23:34:08 +0100

    Checkout

--- TicketPurchaseAPI/Controllers/PaymentController.cs
+++ TicketPurchaseAPI/Controllers/PaymentController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using System.Text;
 using TicketPurchaseAPI.Dto.Payment;
+using TicketPurchaseAPI.Extensions;
+using TicketPurchaseAPI.Model;
 
 namespace TicketPurchaseAPI.Controllers
 {
@@ -12,19 +17,35 @@ namespace TicketPurchaseAPI.Controllers
     public class PaymentController : ControllerBase
     {
         private readonly IConfiguration _config;
-        public PaymentController(IConfiguration config)
+        private readonly UserManager<AppUser> _userManager;
+
+        public PaymentController(IConfiguration config, UserManager<AppUser> userManager)
         {
             _config = config;
+            _userManager = userManager;
         }
 
+        
         [HttpPost("/checkout")]
-        public async Task<IActionResult> Payment([FromBody] CheckoutPaymentDto checkoutDto )
+        [Authorize]
+        public async Task<IActionResult> Payment([FromBody] int amount )
         {
-            var paymentDetail = new CheckoutPaymentDto();
+
+            var email = User.FindFirstValue(ClaimTypes.Email);
+
+            var paymentDetail = new CheckoutPaymentDto
+            {
+                amount = amount,
+                email = email
+            };
             string json = JsonConvert.SerializeObject(paymentDetail);
-            var content = new StringContent(json,Encoding.UTF8, "application/json"); 
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+
             var client = new HttpClient();
-            var response = client.PostAsync("https://api.flutterwave.com/v3/payments", content).Result;
+            client.BaseAddress = new Uri("https://api.flutterwave.com/v3/payments");
+
+            var response = client.PostAsync("checkout", content).Result;
             if (response.IsSuccessStatusCode)
             {
                 var responseContent = await response.Content.ReadAsStringAsync();
@@ -35,7 +56,6 @@ namespace TicketPurchaseAPI.Controllers
             {
                 return BadRequest(response.StatusCode);
             }
-
         }
 
 
---
+++ TicketPurchaseAPI/Extensions/ClaimsExtension.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace TicketPurchaseAPI.Extensions
+{
+    public static class ClaimsExtension
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")).Value;
+        }
+
+        public static string GetEmail(this ClaimsPrincipal user)
+        {
+            return user.Claims.SingleOrDefault(x => x.Type.Equals(ClaimTypes.Email))?.Value;
+        }
+    }
+}
